18d901c851d02e77972499d97562ac80028d235b
naive555 <non.naive@gmail.com>
2019-02-04 10:57:17 +0700

    Add widget

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -26,45 +26,26 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("TabBar"),
-            bottom: TabBar(tabs: <Widget>[
-              Tab(
-               icon: Icon(Icons.camera), text: "Camera",
-              ),
-              Tab(
-                icon: Icon(Icons.add_alarm),
-              ),
-              Tab(
-                icon: Icon(Icons.add_alarm),
-              )
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            Icon(Icons.camera),
-            Icon(Icons.add_alarm),
-            Icon(Icons.account_box)
-          ],
-        ),
-      ),
-    );
-  }
-}
-// FuckYouGIT
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
 //     // This method is rerun every time setState is called, for instance as done
 //     // by the _incrementCounter method above.
 //     //
@@ -95,5 +76,32 @@ class MyHomePage extends StatefulWidget {
 //         }
 //       ),
 //     );
-//   }
-// }
+  return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("TabBar"),
+            bottom: TabBar(tabs: <Widget>[
+              Tab(
+               icon: Icon(Icons.camera), text: "Camera",
+              ),
+              Tab(
+                icon: Icon(Icons.add_alarm),
+              ),
+              Tab(
+                icon: Icon(Icons.add_alarm),
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.camera),
+            Icon(Icons.add_alarm),
+            Icon(Icons.account_box)
+          ],
+        ),
+      ),
+    );
+  }
+}
